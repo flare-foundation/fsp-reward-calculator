1ef66551692cb07584dafdf0b58020eba42ac747
Andrius Dagys <andrius@flare.network>
2024-10-28 17:41:34 +0200

    Update Coston addresses

--- params/coston.go
+++ params/coston.go
@@ -11,13 +11,13 @@ var coston = Network{
 	Contracts: ContractAddresses{
 		FlareSystemsManager:        common.HexToAddress("0x85680Dd93755Fe5d0789773fd0896cEE51F9e358"),
 		FtsoRewardOffersManager:    common.HexToAddress("0xC9534cB913150aD3e98D792857689B55e2404212"),
-		RewardManager:              common.HexToAddress("0xA17197b7Bdff7Be7c3Da39ec08981FB716B70d3A"),
+		RewardManager:              common.HexToAddress("0x2ade9972E7f27200872D378acF7a1BaD8D696FC5"),
 		Submission:                 common.HexToAddress("0x2cA6571Daa15ce734Bbd0Bf27D5C9D16787fc33f"),
-		Relay:                      common.HexToAddress("0x32D46A1260BB2D8C9d5Ab1C9bBd7FF7D7CfaabCC"),
+		Relay:                      common.HexToAddress("0x92a6E1127262106611e1e129BB64B6D8654273F7"),
 		FlareSystemsCalculator:     common.HexToAddress("0x43CBAB9C953F54533aadAf7ffCD13c30ec05Edc9"),
 		VoterRegistry:              common.HexToAddress("0xE2c06DF29d175Aa0EcfcD10134eB96f8C94448A3"),
 		FastUpdateIncentiveManager: common.HexToAddress("0x8c45666369B174806E1AB78D989ddd79a3267F3b"),
-		FastUpdater:                common.HexToAddress("0x9B931f5d3e24fc8C9064DB35bDc8FB4bE0E862f9"),
+		FastUpdater:                common.HexToAddress("0xB8336A96b4b8af89f60EA080002214191Bc8293A"),
 	},
 
 	InitialRewardEpochId: 2466,
