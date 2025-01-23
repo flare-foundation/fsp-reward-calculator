9e6761c80569e0164eccb08de7457cc1d1b3831c
Andrius Dagys <andrius@flare.network>
2025-01-23 14:55:06 +0200

    Update Relay address for FLR

--- params/flare.go
+++ params/flare.go
@@ -13,7 +13,7 @@ var flare = Network{
 		FtsoRewardOffersManager:    common.HexToAddress("0x244EA7f173895968128D5847Df2C75B1460ac685"),
 		RewardManager:              common.HexToAddress("0xC8f55c5aA2C752eE285Bd872855C749f4ee6239B"),
 		Submission:                 common.HexToAddress("0x2cA6571Daa15ce734Bbd0Bf27D5C9D16787fc33f"),
-		Relay:                      common.HexToAddress("0xea077600E3065F4FAd7161a6D0977741f2618eec"),
+		Relay:                      common.HexToAddress("0x57a4c3676d08Aa5d15410b5A6A80fBcEF72f3F45"),
 		FlareSystemsCalculator:     common.HexToAddress("0x67c4B11c710D35a279A41cff5eb089Fe72748CF8"),
 		VoterRegistry:              common.HexToAddress("0x2580101692366e2f331e891180d9ffdF861Fce83"),
 		FastUpdateIncentiveManager: common.HexToAddress("0xd648e8ACA486Ce876D641A0F53ED1F2E9eF4885D"),
