1c19ac3b8aaa967b92fe768b8e26c5236ed7f515
Andrius Dagys <andrius@flare.network>
2025-05-20 12:03:04 +0300

    Add missing decoding logic

---
+++ common/fsp/decoding.go
@@ -0,0 +1,186 @@
+package fsp
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"fsp-rewards-calculator/common/ty"
+	"fsp-rewards-calculator/logger"
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/flare-foundation/go-flare-common/pkg/policy"
+	"github.com/pkg/errors"
+)
+
+const (
+	protocolMerkleRootBytes = 38
+	signatureBytes          = 65
+)
+
+// DecodeSignatureType0 decodes a type 0 signature message. Type 0 signatures are deprecated, so we decode but
+// skip the additional Merkle root payload.
+func DecodeSignatureType0(bytes []byte) (*RawSignature, error) {
+	if len(bytes) < 1+protocolMerkleRootBytes+signatureBytes {
+		return nil, errors.Errorf("Type 0 signature message too short: %s", bytes)
+	}
+
+	if bytes[0] != 0 {
+		logger.Fatal("invalid signature type: %d, expected 0", bytes[0])
+	}
+	p := 1
+	encodedMerkleRoot := bytes[p : p+protocolMerkleRootBytes]
+	p += protocolMerkleRootBytes
+	signature := bytes[p : p+signatureBytes]
+	p += signatureBytes
+
+	_, err := DecodeProtocolMerkleRoot(encodedMerkleRoot)
+	if err != nil {
+		return nil, errors.Wrap(err, "error decoding protocol merkle merkleRoot")
+	}
+
+	return &RawSignature{
+		Bytes:   signature,
+		Message: bytes[p:],
+	}, nil
+}
+
+func DecodeSignatureType1(bytes []byte) (*RawSignature, error) {
+	if len(bytes) < 1+signatureBytes {
+		return nil, errors.Errorf("Type 1 signature message too short: %s", bytes)
+	}
+
+	if bytes[0] != 1 {
+		return nil, errors.Errorf("invalid signature type: %d, expected 1", bytes[0])
+	}
+	p := 1
+	signature := bytes[p : p+signatureBytes]
+	p += signatureBytes
+	unsignedMessage := bytes[p:]
+
+	return &RawSignature{
+		Bytes:   signature,
+		Message: unsignedMessage,
+	}, nil
+}
+
+func DecodeFinalization(hexMessage string) (*Finalization, error) {
+	bytes, err := hex.DecodeString(hexMessage)
+	if err != nil {
+		return nil, errors.Wrapf(err, "message is not a valid hex string: %s", hexMessage)
+	}
+	signingPolicy, p, err := policy.FromRawBytes(bytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "error decoding signing policy")
+	}
+	protocol := bytes[p] // Peek ahead to the protocol ID but don't process it
+	if protocol == 0 {
+		// This is a new signing policy message, ignore
+		return nil, nil
+	}
+
+	merkleRootBytes := bytes[p : p+protocolMerkleRootBytes]
+	merkleRootHash := accounts.TextHash(crypto.Keccak256(merkleRootBytes))
+	merkleRoot, err := DecodeProtocolMerkleRoot(merkleRootBytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "error decoding protocol merkle merkleRoot")
+	}
+	p += protocolMerkleRootBytes
+
+	signatureCount := int(binary.BigEndian.Uint16(bytes[p : p+2]))
+	p += 2
+
+	if signatureCount > len(signingPolicy.Voters.VoterDataMap) {
+		return nil, errors.Errorf("signature count %d exceeds number of signing policy voters %d", signatureCount, len(signingPolicy.Voters.VoterDataMap))
+	}
+
+	var signatures []ECDSASignatureWithIndex
+	signatureWeight := uint16(0)
+
+	for i := 0; i < signatureCount; i++ {
+		v := bytes[p] - 27
+		p++
+		r := bytes[p : p+32]
+		p += 32
+		s := bytes[p : p+32]
+		p += 32
+		index := binary.BigEndian.Uint16(bytes[p : p+2])
+		p += 2
+
+		signature := ECDSASignatureWithIndex{
+			V:           v,
+			R:           [32]byte(r),
+			S:           [32]byte(s),
+			signerIndex: index,
+		}
+
+		if i > 0 && index <= signatures[i-1].signerIndex {
+			return nil, errors.Errorf("signature index %d is not greater than previous index %d", signatures[i].signerIndex, signatures[i-1].signerIndex)
+		}
+
+		actualSigner, err := crypto.SigToPub(
+			merkleRootHash,
+			signature.Bytes(),
+		)
+		if err != nil {
+			logger.Info("error recovering signer from signature: ", err)
+			continue
+		}
+		expectedSigner := signingPolicy.Voters.VoterAddress(int(index))
+
+		if expectedSigner != crypto.PubkeyToAddress(*actualSigner) {
+			logger.Info("signature at index %d does not match expected signer: %s", index, expectedSigner)
+			continue
+		}
+
+		signatureWeight += signingPolicy.Voters.VoterWeight(int(index))
+
+		signatures = append(signatures, signature)
+	}
+
+	if signatureWeight <= signingPolicy.Threshold {
+		return nil, errors.Errorf("total signature weight %d is less than threshold %d", signatureWeight, signingPolicy.Threshold)
+	}
+
+	return &Finalization{
+		Policy:     *signingPolicy,
+		MerkleRoot: merkleRoot,
+		Signatures: signatures,
+	}, nil
+}
+
+func DecodeProtocolMerkleRoot(bytes []byte) (ProtocolMerkleRoot, error) {
+	if len(bytes) != protocolMerkleRootBytes {
+		return ProtocolMerkleRoot{}, errors.New("invalid message length for protocol merkle merkleRoot")
+	}
+	p := 0
+	id := bytes[p]
+	p++
+	round := ty.RoundId(DecodeUint32(bytes[p : p+4]))
+	p += 4
+	isSecureRandom := bytes[p] == 1
+	p++
+	merkleRoot := common.BytesToHash(bytes[p : p+common.HashLength])
+
+	encoded := [protocolMerkleRootBytes]byte{}
+	copy(encoded[:], bytes)
+
+	return ProtocolMerkleRoot{
+		ProtocolId:     int8(id),
+		Round:          round,
+		IsSecureRandom: isSecureRandom,
+		Hash:           merkleRoot,
+		rawEncoded:     encoded,
+	}, nil
+}
+
+// DecodeUint32 decodes a big-endian uint32 from a variable length byte slice of up to 4 bytes.
+func DecodeUint32(bytes []byte) uint32 {
+	if len(bytes) > 4 {
+		logger.Fatal("invalid length for decode int: %d", len(bytes))
+	}
+
+	start := 4 - len(bytes)
+	var tmp = make([]byte, 4)
+	copy(tmp[start:], bytes)
+	return binary.BigEndian.Uint32(tmp[:])
+}
---
+++ common/ftso/decoding.go
@@ -0,0 +1,189 @@
+package ftso
+
+import (
+	"fsp-rewards-calculator/common/fsp"
+	"fsp-rewards-calculator/common/params"
+	"fsp-rewards-calculator/common/ty"
+	"fsp-rewards-calculator/logger"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/flare-foundation/go-flare-common/pkg/payload"
+	"github.com/pkg/errors"
+)
+
+func DecodeFeedValues(bytes []byte, feeds []fsp.Feed) ([]FeedValue, error) {
+	if (len(bytes) % feedValueBytes) != 0 {
+		return nil, errors.New("invalid message length for feed values")
+	}
+
+	var feedValues []FeedValue
+	for i := 0; i < len(bytes); i += feedValueBytes {
+		rawValue := fsp.DecodeUint32(bytes[i : i+feedValueBytes])
+
+		var feedValue FeedValue
+		if rawValue == noValue {
+			feedValue = EmptyFeedValue
+		} else {
+			feedValue = FeedValue{
+				IsEmpty: false,
+				Value:   int32(rawValue - 1<<31), // Values encoded in Excess-2^31
+			}
+		}
+		feedValues = append(feedValues, feedValue)
+	}
+
+	// Fill in values for truncated empty feeds
+	for i := len(feedValues); i < len(feeds); i++ {
+		feedValues = append(feedValues, EmptyFeedValue)
+	}
+
+	return feedValues, nil
+}
+
+func DecodeCommit(bytes []byte) (*Commit, error) {
+	if len(bytes) != common.HashLength {
+		return nil, errors.New("invalid message length")
+	}
+	hash := common.BytesToHash(bytes)
+	return &Commit{
+		Hash: hash,
+	}, nil
+
+}
+
+func DecodeReveal(bytes []byte, expectedFeeds int) (*Reveal, error) {
+	// The message should be long enough to contain the random and at least one feed value
+	if len(bytes) < (common.HashLength + feedValueBytes) {
+		return nil, errors.New("message too short")
+	}
+
+	random := common.BytesToHash(bytes[:common.HashLength])
+	encodedFeeds := bytes[common.HashLength:]
+
+	if (len(encodedFeeds) % feedValueBytes) != 0 {
+		return nil, errors.Errorf("invalid message length %d for feed values", len(encodedFeeds))
+	}
+	if (len(encodedFeeds) / feedValueBytes) > expectedFeeds {
+		return nil, errors.Errorf("encoded feed values payload %d exceeds expected number of feeds %d", len(encodedFeeds)/feedValueBytes, expectedFeeds)
+	}
+
+	return &Reveal{
+		Random:        random,
+		EncodedValues: encodedFeeds,
+	}, nil
+}
+
+func ExtractCommits(messages []payload.Message) (map[ty.RoundId]map[ty.VoterSubmit]*Commit, error) {
+	var commitsByRound = map[ty.RoundId]map[ty.VoterSubmit]*Commit{}
+	for _, msg := range messages {
+		if msg.ProtocolID != ProtocolId {
+			logger.Fatal("message protocol %d does not match expected protocol %d - likely an issue with submission retrieval")
+		}
+
+		round := ty.RoundId(msg.VotingRound)
+		submitRound := params.Net.Epoch.VotingRoundForTimeSec(msg.Timestamp)
+		if round != submitRound {
+			logger.Debug("commit round %d does not match expected round %d, skipping", round, submitRound)
+			continue
+		}
+
+		commit, err := DecodeCommit(msg.Payload)
+		if err != nil {
+			logger.Debug("error parsing commit, skipping: %s", err)
+			continue
+		}
+
+		if _, ok := commitsByRound[round]; !ok {
+			commitsByRound[round] = map[ty.VoterSubmit]*Commit{}
+		}
+
+		from := ty.VoterSubmit(msg.From)
+		commitsByRound[round][from] = commit
+	}
+
+	return commitsByRound, nil
+}
+
+func ExtractReveals(messages []payload.Message, getRoundEpoch func(ty.RoundId) *fsp.RewardEpoch) (map[ty.RoundId]map[ty.VoterSubmit]*Reveal, error) {
+	var revealsByRound = map[ty.RoundId]map[ty.VoterSubmit]*Reveal{}
+	for _, msg := range messages {
+		if msg.ProtocolID != ProtocolId {
+			logger.Fatal("message protocol %d does not match expected protocol %d - likely an issue with submission retrieval")
+		}
+
+		round := ty.RoundId(msg.VotingRound)
+		submitRound := params.Net.Epoch.VotingRoundForTimeSec(msg.Timestamp)
+		if round != submitRound.Add(-1) {
+			logger.Debug("reveal round %d does not match expected round %d, skipping", round, submitRound.Add(-1))
+			continue
+		}
+
+		if msg.Timestamp > params.Net.Epoch.RevealDeadlineSec(submitRound) {
+			logger.Debug("reveal from %s too late", msg.From)
+			continue
+		}
+
+		feeds := getRoundEpoch(round).OrderedFeeds
+		reveal, err := DecodeReveal(msg.Payload, len(feeds))
+		if err != nil {
+			logger.Debug("error parsing reveal, skipping: %s", err)
+			continue
+		}
+
+		if _, ok := revealsByRound[round]; !ok {
+			revealsByRound[round] = map[ty.VoterSubmit]*Reveal{}
+		}
+
+		from := ty.VoterSubmit(msg.From)
+		revealsByRound[round][from] = reveal
+	}
+
+	return revealsByRound, nil
+}
+
+func ExtractSignatures(messages []payload.Message) (map[ty.RoundId][]*SignatureSubmission, error) {
+	var signaturesByRound = map[ty.RoundId][]*SignatureSubmission{}
+	for _, msg := range messages {
+		if msg.ProtocolID != ProtocolId {
+			logger.Fatal("message protocol %d does not match expected protocol %d - likely an issue with submission retrieval")
+		}
+
+		round := ty.RoundId(msg.VotingRound)
+		expectedRound := params.Net.Epoch.VotingRoundForTimeSec(msg.Timestamp) - 1
+		if round != expectedRound {
+			logger.Debug("Signature round %d does not match expected round %d, skipping", round, expectedRound)
+			continue
+		}
+
+		var signature *fsp.RawSignature
+		var err error
+
+		if msg.Payload[0] == 1 {
+			signature, err = fsp.DecodeSignatureType1(msg.Payload)
+			if err != nil {
+				logger.Debug("error parsing Signature, skipping: %signature", err)
+				continue
+			}
+		} else {
+			signature, err = fsp.DecodeSignatureType0(msg.Payload)
+			if err != nil {
+				logger.Debug("error parsing Signature, skipping: %signature", err)
+				continue
+			}
+		}
+
+		if _, ok := signaturesByRound[round]; !ok {
+			signaturesByRound[round] = []*SignatureSubmission{}
+		}
+
+		signaturesByRound[round] = append(signaturesByRound[round], &SignatureSubmission{
+			Signature: signature,
+			Info: fsp.TxInfo{
+				From:         msg.From,
+				TimestampSec: msg.Timestamp,
+				Reverted:     false,
+			},
+		})
+	}
+
+	return signaturesByRound, nil
+}
